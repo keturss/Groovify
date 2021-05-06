b5761268e1d7ef59d7da5bca96ba5e21e80e9773
elleriche <elleriche@8159da03-380d-4177-8a1a-efeefe33feab>
2021-05-06 19:28:05 +0000

    Ajout fenetre playlist dans le prog + debut de la personnalisation des barres du haut

---
+++ trunc/src/Groovify/Groovify/popup/NouvellePlaylist.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Groovify.popup
+{
+    /// <summary>
+    /// Logique d'interaction pour NouvellePlaylist.xaml
+    /// </summary>
+    public partial class NouvellePlaylist : Window
+    {
+        public NouvellePlaylist()
+        {
+            InitializeComponent();
+        }
+        private void ValiderNouvellePlaylist(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void AnnulerNouvellePlaylist(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
