a26c54ed4f7460ebe8bb1511371c230934564a04
elleriche <elleriche@8159da03-380d-4177-8a1a-efeefe33feab>
2021-05-12 14:07:55 +0000

    added files to ignore (bin, obj, .vs)

---
+++ trunc/src/Groovify/Groovify/Vues/VueDetailArtiste.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Groovify.Vues
+{
+    /// <summary>
+    /// Logique d'interaction pour VueDetailArtiste.xaml
+    /// </summary>
+    public partial class VueDetailArtiste : UserControl
+    {
+        public VueDetailArtiste()
+        {
+            InitializeComponent();
+        }
+    }
+}
