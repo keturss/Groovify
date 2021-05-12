a0f4b1897398f6f8bfb61245ffe32ee62d275c80
Keturss <46556594+keturss@users.noreply.github.com>
2021-05-12 14:22:32 +0000

    1 first commit Modele

---
+++ trunc/src/Groovify/Modele/Artiste.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    class Artiste
+    {
+        public String Name { get; set; }
+        private String Description { get; set; }
+        private String Image { get; set; }
+        public Artiste NameArtiste { get; set; }
+
+        public Artiste(String name)
+        {
+            Name = name;
+        }
+
+
+    }
+}
---
+++ trunc/src/Groovify/Modele/Playlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    class Playlist
+    {
+        private String Name { get; set; }
+        private List<Musique> musiquePlaylist = new List<Musique>();
+
+        public void addMusic(Musique musique)
+        {
+            musiquePlaylist.Add(musique);
+        }
+
+        public void deleteMusic(Musique musique)
+        {
+            musiquePlaylist.Remove(musique);
+        }
+
+
+    }
+}
