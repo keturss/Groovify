52cf233220eed0e75de86b10fa76aaf590c3ea36
Keturss <46556594+keturss@users.noreply.github.com>
2021-05-17 17:26:07 +0000

    Modele update 1

--- trunc/src/Groovify/Modele/Manager.cs
+++ trunc/src/Groovify/Modele/Manager.cs
@@ -4,10 +4,34 @@ using System.Text;
 
 namespace Modele
 {
-    class Manager
+    public class Manager
     {
+        List<Artiste> ListeArtiste = new List<Artiste>();
 
-        public List<Artiste> ListeArtiste = new List<Artiste>();
-        public List<Playlist> ListePlaylist = new List<Playlist>();
+
+        public List<Musique> AllMusic()
+        {
+            List<Musique> ListeOeuvre = new List<Musique>();
+            
+            foreach(Artiste aRtiste in ListeArtiste)
+            {
+                foreach (Album aLbum in ListeAlbum)
+                {
+                    foreach (Musique mUsique in ListeMusique)
+                    {
+                        ListeOeuvre.add(mUsique);
+                    }
+                }
+            }
+            
+
+            return ListeOeuvre;
+        }
+
+
+        public Artiste rechercheArtiste(string name)
+        {
+
+        }
     }
 }
--- trunc/src/Groovify/Modele/Musique.cs
+++ trunc/src/Groovify/Modele/Musique.cs
@@ -10,9 +10,36 @@ namespace Modele
         public String Name { get; set; }
         public int Lenght { get; set; }
         public String Path { get; set; }
-        public int Date { get; set; }
+        public DateTime Date { get; set; }
         public String NameAlbum { get; set; }
         public String NameArtiste { get; set; }
 
+        public Musique(String name, String nomArtiste, String nomAlbum, List<Artiste> ListeArtiste, List<Album> ListeAlbum)
+        {
+            Name = name;
+            NameAlbum = nomAlbum;
+            NameArtiste = nomArtiste;
+
+
+            int index = ListeAlbum.FindIndex(x => x.Name == nomAlbum);
+            if (index < 0) // Si le nom de l album n'existe pas 
+            {
+                //Creation de l'album
+                ListeAlbum.Add(new Album(nomAlbum,ListeArtiste, nomArtiste));
+                Console.WriteLine("Creation Album");
+            }
+        }
+        public Musique() { }
+
+        public override string ToString()
+        {
+            return "Musique => Nom : " + Name;
+        }
     }
+
+
+
+    
+
+    
 }
